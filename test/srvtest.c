8cde6e24bc88daf1c823d2e7b6f43ab7393ed113
Jeff Nettleton <jeffdn@gmail.com>
2011-10-01 00:59:27 -0700

    improvements to test suite. x threads make y connections to srv.

--- test/srvtest.c
+++ test/srvtest.c
@@ -15,15 +15,22 @@
 #define SRV_TEST_LEVEL 8
 #define SRV_TEST_ITERS 1024
 
+static sock_t sock[SRV_TEST_LEVEL];
+static pthread_t p[SRV_TEST_LEVEL];
+static pthread_mutex_t mt[SRV_TEST_LEVEL];
+
 void *srv_test_handler(void *arg)
 {
     char *request;
     char buf[1024];
-    unsigned int i;
-    sock_t *s = (sock_t *)arg;
+    unsigned int i, id;
+    sock_t *s;
 
+    id = (unsigned int)arg;
+    s = &sock[id];
+
+    pthread_mutex_lock(&mt[id]);
     request = strdup("GET / HTTP/1.0\r\n\r\n");
-    printf("request: %s", request);
 
     for (i = 0; i < SRV_TEST_ITERS; i++) {
         memset(buf, '\0', sizeof buf);
@@ -40,10 +47,11 @@ void *srv_test_handler(void *arg)
             ERRF(__FILE__, __LINE__, "getting response!\n");
 
         sock_disconnect(s);
-
+        usleep(500);
         printf("%s\n", buf);
     }
 
+    pthread_mutex_unlock(&mt[id]);
     free(request);
 
     return NULL;
@@ -52,10 +60,7 @@ void *srv_test_handler(void *arg)
 int main(int argc, char *argv[])
 {
     char *host;
-    unsigned short port, i;
-
-    sock_t sock[SRV_TEST_LEVEL];
-    pthread_t p[SRV_TEST_LEVEL];
+    unsigned int port, i;
     
     if (argc != 3) {
         ERRF(__FILE__, __LINE__, "./srvtest hostname port\n");
@@ -63,17 +68,22 @@ int main(int argc, char *argv[])
     }
 
     host = strdup(argv[1]);
-    port = (unsigned short) strtol(argv[2], NULL, 0);
+    port = (unsigned int) strtol(argv[2], NULL, 0);
 
     for (i = 0; i < SRV_TEST_LEVEL; i++) {
         /* get these threads rollin' */
         sock_init(&sock[i], SOCK_STREAM, SOCK_TYPE_CLIENT, host, port);
-        pthread_create(&p[i], NULL, srv_test_handler, (void *)&sock[i]);
-        printf("pew!\n");
+        pthread_mutex_init(&mt[i], NULL);
+        pthread_create(&p[i], NULL, srv_test_handler, (void *)i);
+        pthread_detach(p[i]);
     }
 
     free(host);
-    usleep(1000000);
+
+    for (i = 0; i < SRV_TEST_LEVEL; i++) {
+        /* shut it down */
+        pthread_mutex_lock(&mt[i]);
+    }
 
     return 0;
 }
